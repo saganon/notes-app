17877125e905007e93cf9a9d2446b7ced10214f4
saganon <shanonathooya@gmail.com>
2024-01-20 23:33:58 +0700

    add notes model

---
+++ lib/models/notes_model.dart
@@ -0,0 +1,15 @@
+class Notes {
+  int id;
+  String title;
+  String createdAt;
+  String updatedAt;
+  String image;
+
+  Notes({
+    required this.id,
+    required this.title,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.image,
+  });
+}
