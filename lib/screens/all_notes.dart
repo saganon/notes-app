64e24463d94f8e50829f876824be17de702e460d
saganon <shanonathooya@gmail.com>
2024-01-20 22:55:01 +0700

    add dummy image in notes

--- lib/screens/all_notes.dart
+++ lib/screens/all_notes.dart
@@ -50,7 +50,71 @@ class AllNotes extends StatelessWidget {
                       color: Color.fromRGBO(129, 129, 129, 1),
                     ),
                   ),
-                )
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 183,
+            width: MediaQuery.of(context).size.width,
+            margin: const EdgeInsets.only(
+              top: 16.0,
+            ),
+            decoration: const BoxDecoration(
+              color: Color.fromRGBO(247, 247, 247, 1),
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 114.0,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                      image: DecorationImage(
+                        image: AssetImage('images/dummy-notes-img.png'),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(
+                    left: 16.0,
+                    top: 12.0,
+                    bottom: 12.0,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Welcome to Notes',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                          fontFamily: 'Poppins',
+                          color: Color.fromRGBO(0, 0, 0, 1),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(
+                          top: 6.0,
+                        ),
+                        child: const Text(
+                          '15 January 2023',
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            fontFamily: 'Poppins',
+                            color: Color.fromRGBO(129, 129, 129, 1),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           )
