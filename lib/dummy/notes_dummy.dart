b0b5c6828a32d5717deca6d83372db642ed27ac1
saganon <shanonathooya@gmail.com>
2024-04-20 23:17:06 +0700

    using data from dummy

---
+++ lib/dummy/notes_dummy.dart
@@ -0,0 +1,18 @@
+class NotesDummy {
+  static const List<dynamic> notes = [
+    {
+      "id": 1,
+      "title": "Welcome to Notes",
+      "createdAt": "2024-04-19",
+      "updatedAt": "2024-04-20",
+      "image": "images/dummy-notes-img.png",
+    },
+    {
+      "id": 2,
+      "title": "Meeting Notes",
+      "createdAt": "2024-04-15",
+      "updatedAt": "2024-04-25",
+      "image": null,
+    }
+  ];
+}
--- lib/screens/all_notes.dart
+++ lib/screens/all_notes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/dummy/notes_dummy.dart';
 
 class AllNotes extends StatelessWidget {
   const AllNotes({super.key});
@@ -6,122 +7,84 @@ class AllNotes extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(
-        left: 30.0,
-        right: 30.0,
-        top: 28.0,
-      ),
-      child: Column(
-        children: [
-          Container(
-            height: 72,
-            width: MediaQuery.of(context).size.width,
-            padding: const EdgeInsets.only(
-              left: 16.0,
-              top: 12.0,
-              bottom: 12.0,
-            ),
-            decoration: const BoxDecoration(
-              color: Color.fromRGBO(247, 247, 247, 1),
-              borderRadius: BorderRadius.all(Radius.circular(15)),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Welcome to Notes',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w700,
-                    fontFamily: 'Poppins',
-                    color: Color.fromRGBO(0, 0, 0, 1),
-                  ),
+        margin: const EdgeInsets.only(
+          left: 30.0,
+          right: 30.0,
+          top: 28.0,
+        ),
+        child: ListView.builder(
+            itemCount: NotesDummy.notes.length,
+            itemBuilder: (BuildContext context, int index) {
+              var data = NotesDummy.notes[index];
+              return Container(
+                height: 80,
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.only(
+                  top: 16.0,
                 ),
-                Container(
-                  margin: const EdgeInsets.only(
-                    top: 6.0,
-                  ),
-                  child: const Text(
-                    '14 January 2023',
-                    style: TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.w400,
-                      fontFamily: 'Poppins',
-                      color: Color.fromRGBO(129, 129, 129, 1),
-                    ),
-                  ),
+                padding: const EdgeInsets.only(
+                  left: 16.0,
+                  right: 16.0,
+                  top: 12.0,
+                  bottom: 12.0,
                 ),
-              ],
-            ),
-          ),
-          Container(
-            height: 80,
-            width: MediaQuery.of(context).size.width,
-            margin: const EdgeInsets.only(
-              top: 16.0,
-            ),
-            padding: const EdgeInsets.only(
-              left: 16.0,
-              right: 16.0,
-              top: 12.0,
-              bottom: 12.0,
-            ),
-            decoration: const BoxDecoration(
-              color: Color.fromRGBO(247, 247, 247, 1),
-              borderRadius: BorderRadius.all(Radius.circular(15)),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'Meeting Notes',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w700,
-                          fontFamily: 'Poppins',
-                          color: Color.fromRGBO(0, 0, 0, 1),
-                        ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(
-                          top: 6.0,
-                        ),
-                        child: const Text(
-                          '15 January 2023',
-                          style: TextStyle(
-                            fontSize: 12,
-                            fontWeight: FontWeight.w400,
-                            fontFamily: 'Poppins',
-                            color: Color.fromRGBO(129, 129, 129, 1),
+                decoration: const BoxDecoration(
+                  color: Color.fromRGBO(247, 247, 247, 1),
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            data['title'],
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                              fontFamily: 'Poppins',
+                              color: Color.fromRGBO(0, 0, 0, 1),
+                            ),
                           ),
-                        ),
+                          Container(
+                            margin: const EdgeInsets.only(
+                              top: 6.0,
+                            ),
+                            child: Text(
+                              data['updatedAt'],
+                              style: const TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w400,
+                                fontFamily: 'Poppins',
+                                color: Color.fromRGBO(129, 129, 129, 1),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-                Container(
-                  height: 80,
-                  width: 60,
-                  key: const ValueKey("Notes Image"),
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(15)),
-                    image: DecorationImage(
-                      image: AssetImage('images/dummy-notes-img.png'),
-                      fit: BoxFit.fill,
-                      filterQuality: FilterQuality.high,
                     ),
-                  ),
+                    data['image'] != null
+                        ? Container(
+                            height: 80,
+                            width: 60,
+                            key: const ValueKey("Notes Image"),
+                            decoration: BoxDecoration(
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(15)),
+                              image: DecorationImage(
+                                image: AssetImage(data['image']),
+                                fit: BoxFit.fill,
+                                filterQuality: FilterQuality.high,
+                              ),
+                            ),
+                          )
+                        : Container(),
+                  ],
                 ),
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+              );
+            }));
   }
 }
