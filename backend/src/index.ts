469da314d20176fe7b324e0b4f5e2058d0a0a1e0
saganon <shanonathooya@gmail.com>
2024-04-24 00:49:55 +0700

    add api route and call prisma client

---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+import express, { Request, Response } from "express";
+import createError from "http-errors"
+
+const prisma = new PrismaClient()
+
+
+const app = express()
+
+app.use(express.json())
+
+
+app.get('/notes', async (req: Request, res: Response) => {
+    const posts = await prisma.notes.findMany({})
+    res.json(posts)
+})
+
+// handle 404 error
+app.use((req: Request, res: Response, next: Function) => {
+    next(createError(404))
+})
+
+app.listen(3000, () =>
+    console.log(`⚡️[server]: Server is running at https://localhost:3000`)
+)
+
